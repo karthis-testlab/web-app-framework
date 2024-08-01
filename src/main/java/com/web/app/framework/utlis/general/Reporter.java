3821944c6a7db5c884c57cb1700fee5cb1fbd272
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-08-01 23:42:08 +0530

    Added the extent reportdependency

--- src/main/java/com/web/app/framework/utlis/general/Reporter.java
+++ src/main/java/com/web/app/framework/utlis/general/Reporter.java
@@ -1,5 +1,13 @@
 package com.web.app.framework.utlis.general;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+
 public class Reporter {
+	
+	private static ExtentReports extent;
+	private static final ThreadLocal<ExtentTest> parentTest = new ThreadLocal<ExtentTest>();
+	private static final ThreadLocal<ExtentTest> test = new ThreadLocal<ExtentTest>();
+	private static final ThreadLocal<String> testName = new ThreadLocal<String>();
 
 }
