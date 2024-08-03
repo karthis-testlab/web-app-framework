354d8e50fca2afdb4dd0fbfd9e5b8e2f6d67091d
Karthikeyan Rajendran <Karthikeyan_Rajendran@epam.com>
2024-08-03 11:12:07 +0200

    Added steps

---
+++ src/test/java/com/web/app/framework/step/definitions/SauceDemoSteps.java
@@ -0,0 +1,25 @@
+package com.web.app.framework.step.definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class SauceDemoSteps {
+	
+	@Given("User want to purchase the sauce lab merchandise product on the offical cart site")
+	public void user_want_to_purchase_the_sauce_lab_merchandise_product_on_the_offical_cart_site() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("Regitered user able to login with valid user credentials {string} and {string}")
+	public void regitered_user_able_to_login_with_valid_user_credentials_and(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("User able to the see {string} the product they wants")
+	public void user_able_to_the_see_the_product_they_wants(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+}
